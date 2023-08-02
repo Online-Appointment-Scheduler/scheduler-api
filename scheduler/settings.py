25ff7914d27edf3ceec9f72fc828b1ca9447ea46
unknown <ivannikovmax04@gmail.com>
2023-08-02 14:18:14 +0300

    Remove redundant templateHTML renderer

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -50,7 +50,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.TemplateHTMLRenderer',
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
